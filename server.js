78e2930286bcc93863838ee46064665f5a49c17f
debra1761 <dmenezes16@gmail.com>
2020-07-02 11:55:40 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const http=require('http')
+const server=http.createServer((req,res)=>{
+    console.log('New connection')
+    res.end('hello you!')
+})
+const PORT=process.env.PORT ||8080
+server.listen(PORT,() =>console.log('Listening'))
