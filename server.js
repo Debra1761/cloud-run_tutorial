b3c90bfcbc7aeb108258f6c655dd943f56d9d76b
debra1761 <dmenezes16@gmail.com>
2020-07-02 12:56:44 +0200

    modify server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http=require('http')
 const server=http.createServer((req,res)=>{
     console.log('New connection')
-    res.end('hello you!')
+    res.end('hey you!')
 })
 const PORT=process.env.PORT ||8080
 server.listen(PORT,() =>console.log('Listening'))
